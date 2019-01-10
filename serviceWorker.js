8dbc3ff5ffaacefac03aad4cae6e78b4176b8fb4
stepanoff <stepanoff@skbkontur.ru>
2019-01-10 14:21:39 +0500

    store posts data in indexedDB

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,23 +1,30 @@
+importScripts('/src/js/idb.js');
+importScripts('/src/js/idbHelper.js');
+
+var STATIC_FILES = [
+	'/',
+	'/index.html',
+	'/src/js/app.js',
+	'/src/js/idb.js',
+	'/src/js/feed.js',
+	'/src/js/material.min.js',
+	'/src/css/app.css',
+	'/src/css/feed.css',
+	'/src/images/main-image-lg.jpg',
+	'/src/images/main-image-sm.jpg',
+	'/src/images/main-image.jpg',
+	'/src/images/sf-boat.jpg',
+	'https://fonts.googleapis.com/css?family=Roboto:400,700',
+	'https://fonts.googleapis.com/icon?family=Material+Icons',
+	'https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css'
+];
+var POSTS_URL = 'https://pwagram-8fd0d.firebaseio.com/posts';
+
 self.addEventListener('install', function(e) {
     e.waitUntil(
         caches.open('static')
             .then(function(cache) {
-                cache.addAll([
-                    '/',
-                    '/index.html',
-                    '/src/js/app.js',
-                    '/src/js/feed.js',
-                    '/src/js/material.min.js',
-                    '/src/css/app.css',
-                    '/src/css/feed.css',
-                    '/src/images/main-image-lg.jpg',
-                    '/src/images/main-image-sm.jpg',
-                    '/src/images/main-image.jpg',
-                    '/src/images/sf-boat.jpg',
-                    'https://fonts.googleapis.com/css?family=Roboto:400,700',
-                    'https://fonts.googleapis.com/icon?family=Material+Icons',
-                    'https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css'
-                ])
+	             cache.addAll(STATIC_FILES)
             })
     )
 });
@@ -29,18 +36,11 @@ self.addEventListener('activate', function(e) {
 self.addEventListener('fetch', fetchEventHandler);
 
 function fetchEventHandler(event) {
-    var request = event.request;
-
-    event.respondWith(
-        caches.match(request)
-            .then(function(response) {
-                if (response) {
-                    return response;
-                } else {
-                    return fetchRequest(request);
-                }
-            })
-    )
+    if (event.request.url.indexOf(POSTS_URL) > -1) {
+    	handlePostsFetch(event);
+    } else {
+	    handleCommonFetch(event);
+    }
 }
 
 function fetchRequest(request) {
@@ -51,7 +51,50 @@ function fetchRequest(request) {
 
 function storeInDynamicCache(request, response) {
     return caches.open('dynamic').then(function(cache) {
-        cache.put(request.url, response.clone())
+        cache.put(request.url, response.clone());
         return response;            
     })
+}
+
+function handlePostsFetch(event) {
+	event.respondWith(
+		fetch(event.request).then(function (res) {
+			var clonedRes = res.clone();
+
+			IdbHelper.clearStore('posts')
+				.then(function () {
+					return clonedRes.json();
+				})
+				.then(function (data) {
+					storeDataInDb(data);
+			});
+
+			return res;
+		})
+	)
+}
+
+function handleCommonFetch(event) {
+	var request = event.request;
+
+	event.respondWith(
+		caches.match(request)
+			.then(function(response) {
+				if (response) {
+					return response;
+				} else {
+					return fetchRequest(request);
+				}
+			})
+	)
+}
+
+function storeDataInDb(dataObject) {
+	for (const key in dataObject) {
+		if (dataObject.hasOwnProperty(key)) {
+			const element = dataObject[key];
+
+			IdbHelper.writeData('posts', element);
+		}
+	}
 }
--- src/js/feed.js
+++ src/js/feed.js
@@ -1,70 +1,81 @@
-;(function() {
-  var shareImageButton = document.querySelector('#share-image-button');
-  var createPostArea = document.querySelector('#create-post');
-  var closeCreatePostModalButton = document.querySelector('#close-create-post-modal-btn');
-  var sharedMomentsArea = document.querySelector('#shared-moments');
+;(function () {
+	var shareImageButton = document.querySelector('#share-image-button');
+	var createPostArea = document.querySelector('#create-post');
+	var closeCreatePostModalButton = document.querySelector('#close-create-post-modal-btn');
+	var sharedMomentsArea = document.querySelector('#shared-moments');
 
-  function openCreatePostModal() {
-    createPostArea.style.display = 'block';
-  }
+	function openCreatePostModal() {
+		createPostArea.style.display = 'block';
+	}
 
-  function closeCreatePostModal() {
-    createPostArea.style.display = 'none';
-  }
+	function closeCreatePostModal() {
+		createPostArea.style.display = 'none';
+	}
 
-  shareImageButton.addEventListener('click', openCreatePostModal);
+	shareImageButton.addEventListener('click', openCreatePostModal);
 
-  closeCreatePostModalButton.addEventListener('click', closeCreatePostModal);
+	closeCreatePostModalButton.addEventListener('click', closeCreatePostModal);
 
-  function createCard(cardData) {
-    var cardWrapper = document.createElement('div');
-    cardWrapper.className = 'shared-moment-card mdl-card mdl-shadow--2dp';
-    var cardTitle = document.createElement('div');
-    cardTitle.className = 'mdl-card__title';
-    cardTitle.style.backgroundImage = 'url(' + cardData.image + ')';
-    cardTitle.style.backgroundSize = 'cover';
-    cardTitle.style.height = '180px';
-    cardWrapper.appendChild(cardTitle);
-    var cardTitleTextElement = document.createElement('h2');
-    cardTitleTextElement.className = 'mdl-card__title-text';
-    cardTitleTextElement.textContent = cardData.title;
-    cardTitle.appendChild(cardTitleTextElement);
-    var cardSupportingText = document.createElement('div');
-    cardSupportingText.className = 'mdl-card__supporting-text';
-    cardSupportingText.textContent = cardData.location;
-    cardSupportingText.style.textAlign = 'center';
-    cardWrapper.appendChild(cardSupportingText);
-    componentHandler.upgradeElement(cardWrapper);
-    sharedMomentsArea.appendChild(cardWrapper);
-  }
+	function createCard(cardData) {
+		var cardWrapper = document.createElement('div');
+		cardWrapper.className = 'shared-moment-card mdl-card mdl-shadow--2dp';
+		var cardTitle = document.createElement('div');
+		cardTitle.className = 'mdl-card__title';
+		cardTitle.style.backgroundImage = 'url(' + cardData.image + ')';
+		cardTitle.style.backgroundSize = 'cover';
+		cardTitle.style.height = '180px';
+		cardWrapper.appendChild(cardTitle);
+		var cardTitleTextElement = document.createElement('h2');
+		cardTitleTextElement.className = 'mdl-card__title-text';
+		cardTitleTextElement.textContent = cardData.title;
+		cardTitle.appendChild(cardTitleTextElement);
+		var cardSupportingText = document.createElement('div');
+		cardSupportingText.className = 'mdl-card__supporting-text';
+		cardSupportingText.textContent = cardData.location;
+		cardSupportingText.style.textAlign = 'center';
+		cardWrapper.appendChild(cardSupportingText);
+		componentHandler.upgradeElement(cardWrapper);
+		sharedMomentsArea.appendChild(cardWrapper);
+	}
 
-  fetch('https://pwagram-8fd0d.firebaseio.com/posts.json')
-    .then(function(res) {
-      return res.json();
-    })
-    .then(function(data) {
-      updateUi(normalizeData(data));
-    });
+	fetch('https://pwagram-8fd0d.firebaseio.com/posts.json')
+		.then(function (res) {
+			return res.json();
+		})
+		.then(function (data) {
+			updateUi(normalizeData(data));
+		})
+		.catch(function () {
+			fetchPostsFromDB().then(function (data) {
+				updateUi(data);
+			})
+		});
 
-  function updateUi(data) {
-    for (const cardData of data) {
-      createCard(cardData);
-    }
-  }
+	function updateUi(data) {
+		for (const cardData of data) {
+			createCard(cardData);
+		}
+	}
 
-  function normalizeData(dataObject) {
-    const arrayData = [];
+	function normalizeData(dataObject) {
+		const arrayData = [];
 
-    for (const key in dataObject) {
-      if (dataObject.hasOwnProperty(key)) {
-        const element = dataObject[key];
-        
-        arrayData.push(element);
-      }
-    }
+		for (const key in dataObject) {
+			if (dataObject.hasOwnProperty(key)) {
+				const element = dataObject[key];
 
-    return arrayData;
-  }
-    
-  
+				arrayData.push(element);
+			}
+		}
+
+		return arrayData;
+	}
+
+	function fetchPostsFromDB() {
+		if ('indexedDB' in window) {
+			return IdbHelper.readData('posts')
+		} else {
+			return Promise.resolve([]);
+		}
+	}
 })();
