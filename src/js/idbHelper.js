2da03bc34501db09b6d3036ea7449cfc506c474a
stepanoff <stepanoff@skbkontur.ru>
2019-01-10 14:21:01 +0500

    added idbHelper

---
+++ src/js/idbHelper.js
@@ -0,0 +1,52 @@
+
+function IdbHelperConstructor() {
+	var idbPromise = idb.open('posts-store', 1, function (db) {
+		if (!db.objectStoreNames.contains('posts')) {
+			db.createObjectStore('posts', {keyPath: 'id'});
+		}
+	});
+
+	this.writeData = function (storeName, data) {
+		return idbPromise.then(function (db) {
+			var transaction = db.transaction(storeName, 'readwrite');
+			var store = transaction.objectStore(storeName);
+
+			store.put(data);
+
+			return transaction.complete;
+		});
+	};
+
+	this.readData = function (storeName) {
+		return idbPromise.then(function (db) {
+			var transaction = db.transaction(storeName, 'readonly');
+			var store = transaction.objectStore(storeName);
+
+			return store.getAll();
+		})
+	};
+
+	this.clearStore = function (storeName) {
+		return idbPromise.then(function (db) {
+			var transaction = db.transaction(storeName, 'readwrite');
+			var store = transaction.objectStore(storeName);
+
+			store.clear();
+
+			return transaction.complete;
+		})
+	};
+
+	this.deleteItem = function (storeName, id) {
+		return idbPromise.then(function (db) {
+			var transaction = db.transaction(storeName, 'readwrite');
+			var store = transaction.objectStore(storeName);
+
+			store.delete(id);
+
+			return transaction.complete;
+		})
+	}
+}
+
+var IdbHelper = new IdbHelperConstructor();
